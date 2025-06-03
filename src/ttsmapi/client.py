4afe21c435dc8fef01b495ac732eef588a6f0d2a
cdr <58159483+cdrg@users.noreply.github.com>
2025-06-02 18:04:59 -0600

    Add user_info dict as an attribute and use it instead of separate variables
    Move some attributes to class attributes.

--- src/ttsmapi/client.py
+++ src/ttsmapi/client.py
@@ -2,7 +2,6 @@
 
 https://docs.tts.monster/introduction"""
 
-from typing import Dict
 from importlib.metadata import version
 from datetime import timedelta
 import time
@@ -28,6 +27,14 @@ class Client:
     
     The 'voice-cloning' endpoint is not currently implemented because of its beta status and complexity."""
 
+    _name: str = "Python TTSM-API"
+    #TODO: move rate limiting to a real library like token-throttle later
+    _rate_limit_store: Store = Store()
+    user_rate_limit: RateLimit = RateLimit(count=50, period=timedelta(seconds=60))
+    voices_rate_limit: RateLimit = RateLimit(count=50, period=timedelta(seconds=60))
+    generate_rate_limit: RateLimit = RateLimit(count=30, period=timedelta(seconds=60))
+    generate_character_limit: int = 500
+
     def __init__(self, api_key: str, enforce_char_quota: bool = True):
         """
         Initialize a TTS.Monster API Client object.
@@ -44,48 +51,48 @@ class Client:
         self._retries: int = 3
         self._api_key: str = api_key
         self._version: str = version("ttsmapi")
-        self._name: str = "Python TTSM-API"
         self._user_agent: str = f"{self._name}/{self._version}"
-        #TODO: move rate limiting to a real library like token-throttle later
-        self._rate_limit_store: Store = Store()
-        self.user_rate_limit: RateLimit = RateLimit(count=50, period=timedelta(seconds=60))
-        self.voices_rate_limit: RateLimit = RateLimit(count=50, period=timedelta(seconds=60))
-        self.generate_rate_limit: RateLimit = RateLimit(count=30, period=timedelta(seconds=60))
-        self.generate_character_limit: int = 500
+        
         self.enforce_char_quota: bool = enforce_char_quota
-        self.generate_character_quota: int
-        self.generate_character_usage: int
+
+        # "current_plan": "free",
+        # "status": "active",
+        # "renewal_time": 1727392003,
+        # "character_usage": 145892,
+        # "character_allowance": 500000,
+        # "portal_url": "https://billing.stripe.com/p/session/live_12345",
+        # "has_payment_method": true,
+        # "downgrading_to_plan": null
+        self.user_info: dict
 
         if self.enforce_char_quota:
             try:
-                user_info: Dict = self.get_user()
+                self.user_info = self.get_user()
             except HTTPError as e:
                 if e.response.status_code == HTTPStatus.UNAUTHORIZED:
                     raise TTSMAPIError("Invalid API key") from e
                 raise TTSMAPIError("Failed to get user info from TTS.Monster API") from e
             
-            if 'character_allowance' not in user_info:
+            if 'character_allowance' not in self.user_info:
                 raise TTSMAPIError("User info does not contain 'character_allowance'")
-            self.generate_character_quota = user_info['character_allowance']
-            if 'character_usage' not in user_info:
+            if 'character_usage' not in self.user_info:
                 raise TTSMAPIError("User info does not contain 'character_usage'")
-            self.generate_character_usage = user_info['character_usage']
 
-    def post(self, endpoint: str, rate_limit: RateLimit, ep_json: Dict|None = None) -> Dict:
+    def post(self, endpoint: str, rate_limit: RateLimit, ep_json: dict|None = None) -> dict:
         """Set up and perform a Requests POST for a given TTS.Monster endpoint.
 
         Args:
             endpoint (str): The endpoint to POST to, e.g. 'generate', 'user', 'voices'.
             rate_limit (RateLimit): The RateLimit object for the endpoint.
-            ep_json (Dict|None): The JSON payload to send in the POST request, if any (default None).
+            ep_json (dict|None): The JSON payload to send in the POST request, if any (default None).
         Returns:
-            Dict: The JSON response from the TTS.Monster API.
+            dict: The JSON response from the TTS.Monster API.
         Raises:
             TTSMAPIError: If valid JSON is not in the response.
             HTTPError: If the API returns an unrecoverable non-2XX HTTP status code.
         """
         full_url: str = self._url + endpoint
-        headers: Dict = {'User-Agent': self._user_agent, 'Authorization': self._api_key}
+        headers: dict = {'User-Agent': self._user_agent, 'Authorization': self._api_key}
 
         rate_limited: bool = self._rate_limit_store.update(full_url, rate_limit)
         if rate_limited:
@@ -103,13 +110,13 @@ class Client:
                 raise
 
         try:
-            response_json: Dict = response.json()
+            response_json: dict = response.json()
         except (ValueError, JSONDecodeError) as e:
             raise TTSMAPIError("Invalid/no JSON in response") from e
 
         return response_json
 
-    def generate(self, voice_id: str, message: str, return_usage: bool = True) -> Dict:
+    def generate(self, voice_id: str, message: str, return_usage: bool = True) -> dict:
         """TTS.Monster 'generate' endpoint (POST)
         https://docs.tts.monster/endpoint/generate
 
@@ -118,51 +125,52 @@ class Client:
             message (str): The text to be read.
             return_usage (bool): Whether to return character usage in the response (default True).
         Returns:
-            Dict: The JSON response containing the details of the generated audio.
+            dict: The JSON response containing the details of the generated audio.
         Raises:
             TTSMAPIError: If the message exceeds the character limit, or if the message would exceed the character quota.
             HTTPError: If the API returns an unrecoverable non-2XX HTTP status code.
         """
-        ep_json: Dict = {'voice_id': voice_id,
+        ep_json: dict = {'voice_id': voice_id,
                         'message': message,
                         'return_usage': return_usage}
 
         if len(message) > self.generate_character_limit:
             raise TTSMAPIError(f"Message exceeds character limit of {self.generate_character_limit} characters.")
         if self.enforce_char_quota:
-            if self.generate_character_usage + len(message) > self.generate_character_quota:
+            if self.user_info['character_usage'] + len(message) > self.user_info['character_allowance']:
                 raise TTSMAPIError(f"Message of len {len(message)} would exceed TTSM character usage quota of "
-                                    f"{self.generate_character_usage}/{self.generate_character_quota} characters.")
+                                    f"{self.user_info['character_usage']}/"
+                                    f"{self.user_info['character_allowance']} characters.")
 
-        response_json: Dict = self.post(endpoint="generate", ep_json=ep_json, rate_limit=self.generate_rate_limit)
+        response_json: dict = self.post(endpoint="generate", ep_json=ep_json, rate_limit=self.generate_rate_limit)
 
-        self.generate_character_usage = response_json['characterUsage']
+        self.user_info['character_usage'] = response_json['characterUsage']
 
         return response_json
 
-    def get_user(self) -> Dict:
+    def get_user(self) -> dict:
         """TTS. Monster [get] 'user' endpoint (POST)
         https://docs.tts.monster/endpoint/get-user
 
         Returns:
-            Dict: The JSON response containing the details of the user's subscription.
+            dict: The JSON response containing the details of the user's subscription.
         Raises:
             TTSMAPIError: If valid JSON is not in the response.
             HTTPError: If the API returns an unrecoverable non-2XX HTTP status code.
         """
-        response_json: Dict = self.post(endpoint="user", rate_limit=self.user_rate_limit)
+        response_json: dict = self.post(endpoint="user", rate_limit=self.user_rate_limit)
         return response_json
 
-    def get_voices(self) -> Dict:
+    def get_voices(self) -> dict:
         """TTS.Monster [get] 'voices' endpoint (POST)
         https://docs.tts.monster/endpoint/get-voices
 
         Returns:
-            Dict: The JSON response containing a list of available public voices
+            dict: The JSON response containing a list of available public voices
                   and a list of available private custom voices.
         Raises:
             TTSMAPIError: If valid JSON is not in the response.
             HTTPError: If the API returns an unrecoverable non-2XX HTTP status code.
         """
-        response_json: Dict = self.post(endpoint="voices", rate_limit=self.voices_rate_limit)
+        response_json: dict = self.post(endpoint="voices", rate_limit=self.voices_rate_limit)
         return response_json
